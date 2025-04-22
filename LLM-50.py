00811879264e0b21dabb9563481fd41ac10df328
ZackIonic <zackionic@gmail.com>
2025-04-22 19:01:59 -0400

    Added the python code

---
+++ LLM-50.py
@@ -0,0 +1,51 @@
+# 50 line LLM Made by Zack Crosbie
+
+import random
+import urllib.request
+
+url = "https://raw.githubusercontent.com/ZackIonic/llm-50/refs/heads/main/llm-context"
+reader = urllib.request.urlopen(url).read().decode('utf-8').splitlines()
+
+window = []
+successor_map = {}
+
+for line in reader:
+    line = line.strip('_')
+    for word in line.split():
+        word = word.strip('_')
+        window.append(word)
+        if len(window) == 4:
+            key = (window[0], window[1], window[2])
+            value = window[3]
+            if key not in successor_map:
+                successor_map[key] = [value]
+            else:
+                successor_map[key].append(value)
+            window.pop(0)
+
+while True:
+    words = []
+    sentences = 0
+    while len(words) != 3:
+        words = input("Context: ").split()
+        if len(words) != 3:
+            print('Please input 3 words of context.')
+    word1 = str(words[0])
+    word2 = str(words[1])
+    word3 = str(words[2])
+    length = int(input("Number of sentences: "))
+
+    while True:
+        if ((word1, word2, word3)) in successor_map:
+            print(word1, end=' ')
+            if '.' in word1 or '!' in word1 or '?' in word1:
+                sentences += 1
+                if sentences >= length:
+                    break
+            successors = successor_map[(word1, word2, word3)]
+            word1, word2, word3 = word2, word3, random.choice(successors)
+        else:
+            print("\n ERROR: No matches found for \'" + word1 + " " + word2 + " " + word3 + "\'")
+            break
+
+    print('\n')
